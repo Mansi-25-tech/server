340fc5d9ca65901c80d4af5b01f7962ff92b8381
Mansi-25-tech <itsmansibatham25@gmail.com>
2026-07-20 23:29:24 +0530

    fix cross origin cookie

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -86,8 +86,8 @@ class AdminController {
 
             res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
 });
 
