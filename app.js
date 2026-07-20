2ecd8f7871b96affcc012c3e8cc2421622c97cbd
Mansi-25-tech <itsmansibatham25@gmail.com>
2026-07-20 23:37:43 +0530

    fix cors origin

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
 const express = require("express");
 const app = express();
-
 const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = require("./config/connectDB");
 const web = require("./routes/web");
-
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 
-
-// Database
 connectDB();
 
-
-// CORS (pehle)
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
 
-
-// Body Parser
 app.use(express.json());
 
 app.use(express.urlencoded({
     extended: true
 }));
 
-
-// Cookie
 app.use(cookieParser());
 
-
-// File Upload
 app.use(fileUpload({
-    useTempFiles:true
+    useTempFiles: true
 }));
 
-
-// Routes
 app.use("/api", web);
 
-
-
 app.listen(process.env.PORT, () => {
-    console.log(
-        `Server is running on port ${process.env.PORT}`
-    );
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
